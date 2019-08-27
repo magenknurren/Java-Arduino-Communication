4c5d88168b359fdc1a70a18346e6b683b0018055
root <root@leonard>
2019-08-28 00:19:27 +0200

    little refactoring

--- connect/src/main/java/tommy/arduino/connect/main/App.java
+++ connect/src/main/java/tommy/arduino/connect/main/App.java
@@ -2,6 +2,7 @@ package tommy.arduino.connect.main;
 
 import java.applet.Applet;
 import java.awt.Button;
+import java.awt.TextField;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
@@ -10,90 +11,103 @@ import com.fazecast.jSerialComm.SerialPort;
 
 import tommy.arduino.connect.servo.Servo;
 
-public class App extends Applet{
-	
+public class App extends Applet {
+
 	private static final long serialVersionUID = -1241022805899597942L;
-	
+
 	static SerialPort port;
 
-	public void init() {
-    	
-		final Servo arduinoServo = initArduinoServo();
+	Button setToZero;
+	Button setTo90;
+	Button setTo180;
+	private TextField textArea;
 
-		
-		Button setToZero = new Button();
+	private TextField stateArea;
+
+	private Servo arduinoServo;
+
+	public void init() {
+
+		createUI();
+
+		arduinoServo = initArduinoServo();
+
+		if (arduinoServo != null && arduinoServo.openPort()) {
+			stateArea.setText("connected");
+			setButtonsActive(true);
+		} else {
+			stateArea.setText("could not connect!");
+			setButtonsActive(false);
+		}
+
+	}
+
+	private void createUI() {
+		textArea = new TextField();
+		stateArea = new TextField();
+		setToZero = new Button();
 		setToZero.setLabel("0 degree");
 		setToZero.addActionListener(new ActionListener() {
-			
+
 			public void actionPerformed(ActionEvent e) {
-				// TODO Auto-generated method stub
-				if(arduinoServo.connected()) {
-					try {
-						arduinoServo.setServoDegree(0);
-					} catch (IOException e1) {
-						// TODO Auto-generated catch block
-						e1.printStackTrace();
-					}
+				try {
+					arduinoServo.setServo(0);
+				} catch (IOException e1) {
+					e1.printStackTrace();
 				}
 			}
 		});
-		
-		
-		Button setTo90 = new Button();
+
+		setTo90 = new Button();
 		setTo90.setLabel("90 degree");
 		setTo90.addActionListener(new ActionListener() {
-			
+
 			public void actionPerformed(ActionEvent e) {
-				// TODO Auto-generated method stub
-				if(arduinoServo.connected()) {
-					try {
-						arduinoServo.setServoDegree(90);
-					} catch (IOException e1) {
-						// TODO Auto-generated catch block
-						e1.printStackTrace();
-					}
+				try {
+					arduinoServo.setServo(90);
+				} catch (IOException e1) {
+					e1.printStackTrace();
 				}
 			}
 		});
-		
-		Button setTo180 = new Button();
+
+		setTo180 = new Button();
 		setTo180.setLabel("180 degree");
 		setTo180.addActionListener(new ActionListener() {
-			
+
 			public void actionPerformed(ActionEvent e) {
-				// TODO Auto-generated method stub
-				if(arduinoServo.connected()) {
-					try {
-						arduinoServo.setServoDegree(180);
-					} catch (IOException e1) {
-						// TODO Auto-generated catch block
-						e1.printStackTrace();
-					}
+				try {
+					arduinoServo.setServo(180);
+				} catch (IOException e1) {
+					e1.printStackTrace();
 				}
 			}
 		});
-    	
-    	
-    	add(setToZero);
-    	add(setTo90);
-    	add(setTo180);
-    	
-    }
 
+		add(setToZero);
+		add(setTo90);
+		add(setTo180);
+		add(textArea);
+		add(stateArea);
+	}
 
+	private void setButtonsActive(boolean b) {
+		setToZero.setEnabled(b);
+		setTo90.setEnabled(b);
+		setTo180.setEnabled(b);
+	}
 
 	private Servo initArduinoServo() {
 		SerialPort[] serialPorts = SerialPort.getCommPorts();
 		String arduinoPort = "";
-		
-    	for (SerialPort serialPort : serialPorts) {
-    		if("Arduino Uno".equalsIgnoreCase(serialPort.getDescriptivePortName())) {
-    			System.out.println( "found Arduino on port: " + serialPort.getSystemPortName());
-    			arduinoPort = serialPort.getSystemPortName();
-    			port = SerialPort.getCommPort("//dev//" + arduinoPort);
-    			return new Servo(port);
-    		}
+
+		for (SerialPort serialPort : serialPorts) {
+			if ("Arduino Uno".equalsIgnoreCase(serialPort.getDescriptivePortName())) {
+				arduinoPort = serialPort.getSystemPortName();
+				port = SerialPort.getCommPort("//dev//" + arduinoPort);
+				return new Servo(port);
+			}
 		}
-    	return null;
+		return null;
 	}
 }
